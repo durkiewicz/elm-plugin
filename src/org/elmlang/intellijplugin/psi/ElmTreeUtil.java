bd07c8bea3c842a03d8d924014928e005bee4819
Kamil Durkiewicz <kamil.durkiewicz@siili.fi>
2016-03-17 03:05:14 +0100

    Refactoring of ElmTreeUtil using Java 8.

--- src/org/elmlang/intellijplugin/psi/ElmTreeUtil.java
+++ src/org/elmlang/intellijplugin/psi/ElmTreeUtil.java
@@ -2,13 +2,13 @@ package org.elmlang.intellijplugin.psi;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
-import com.intellij.util.Function;
 import org.elmlang.intellijplugin.psi.impl.ElmPsiElement;
-import org.elmlang.intellijplugin.utils.ListUtils;
 
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 public class ElmTreeUtil {
     public static List<PsiElement> getLeaves(PsiElement element) {
@@ -43,15 +43,15 @@ public class ElmTreeUtil {
                 break;
             } else if (ancestor instanceof ElmCaseOfBranch) {
                 ElmCaseOfBranch caseOfBranch = (ElmCaseOfBranch) ancestor;
-                result.addAll(getDeclarationsFromPattern.fun(caseOfBranch.getPattern()));
+                result.addAll(getDeclarationsFromPattern(caseOfBranch.getPattern()));
             } else if (ancestor instanceof ElmInnerValueDeclaration) {
                 result.addAll(getDeclarationsFromValue((ElmInnerValueDeclaration) ancestor, false));
             } else if (ancestor instanceof ElmLetIn) {
-                result.addAll(getDeclarationsFromLetIn((ElmLetIn)ancestor));
+                result.addAll(getDeclarationsFromLetIn((ElmLetIn) ancestor));
             } else if (ancestor instanceof ElmValueDeclaration) {
-                result.addAll(getDeclarationsFromTopValue((ElmValueDeclaration)ancestor));
+                result.addAll(getDeclarationsFromTopValue((ElmValueDeclaration) ancestor));
             } else if (ancestor instanceof ElmAnonymousFunction) {
-                result.addAll(getDeclarationsFromAnonymousFunction((ElmAnonymousFunction)ancestor));
+                result.addAll(getDeclarationsFromAnonymousFunction((ElmAnonymousFunction) ancestor));
             }
 
             ancestor = ancestor.getParent();
@@ -60,7 +60,11 @@ public class ElmTreeUtil {
     }
 
     private static List<ElmLowerCaseId> getDeclarationsFromAnonymousFunction(ElmAnonymousFunction element) {
-        return ListUtils.flatten(ListUtils.map(element.getPatternList(), getDeclarationsFromPattern));
+        List<ElmLowerCaseId> result = new LinkedList<>();
+        element.getPatternList().stream()
+                .map(ElmTreeUtil::getDeclarationsFromPattern)
+                .forEach(result::addAll);
+        return result;
     }
 
     private static List<ElmLowerCaseId> getDeclarationsFromTopValue(ElmValueDeclaration valueDeclaration) {
@@ -79,7 +83,7 @@ public class ElmTreeUtil {
         addDeclarationsToResult(
                 result,
                 letIn.getInnerValuesList(),
-                getExposedDeclarationsFromInnerValue);
+                v -> getDeclarationsFromValue(v, true));
 
         return result;
     }
@@ -90,7 +94,7 @@ public class ElmTreeUtil {
         ElmOperatorDeclarationLeft operatorLeft;
         List<ElmPattern> patternList = Collections.emptyList();
         if (innerElmValueDeclarationBase.getPattern() != null) {
-            result.addAll(getDeclarationsFromPattern.fun(innerElmValueDeclarationBase.getPattern()));
+            result.addAll(getDeclarationsFromPattern(innerElmValueDeclarationBase.getPattern()));
         } else if ((functionLeft = innerElmValueDeclarationBase.getFunctionDeclarationLeft()) != null) {
             result.add(functionLeft.getLowerCaseId());
             patternList = functionLeft.getPatternList();
@@ -98,109 +102,61 @@ public class ElmTreeUtil {
             patternList = operatorLeft.getPatternList();
         }
         if (!skipInternals) {
-            result.addAll(
-                    ListUtils.flatten(
-                            ListUtils.map(
-                                    patternList,
-                                    getDeclarationsFromPattern
-                            )
-                    )
-            );
+            patternList.stream()
+                    .map(ElmTreeUtil::getDeclarationsFromPattern)
+                    .forEach(result::addAll);
         }
         return result;
     }
 
-    private static final Function<ElmPattern, List<ElmLowerCaseId>> getDeclarationsFromPattern = new Function<ElmPattern, List<ElmLowerCaseId>>() {
-        @Override
-        public List<ElmLowerCaseId> fun(ElmPattern pattern) {
-            if (pattern == null) {
-                return Collections.emptyList();
-            }
-
-            List<ElmLowerCaseId> result = new LinkedList<ElmLowerCaseId>();
-
-            result.addAll(pattern.getLowerCaseIdList());
-
-            addDeclarationsToResult(
-                    result,
-                    pattern.getListPatternList(),
-                    getDeclarationsFromList);
-
-            addDeclarationsToResult(
-                    result,
-                    pattern.getParenthesedPatternList(),
-                    getDeclarationsFromParenthesedPattern);
-
-            addDeclarationsToResult(
-                    result,
-                    pattern.getRecordPatternList(),
-                    getDeclarationsFromRecord);
-
-            addDeclarationsToResult(
-                    result,
-                    pattern.getTuplePatternList(),
-                    getDeclarationsFromTuple);
-
-            addDeclarationsToResult(
-                    result,
-                    pattern.getUnionPatternList(),
-                    getDeclarationsFromUnion);
-
-            return result;
+    private static List<ElmLowerCaseId> getDeclarationsFromPattern(ElmPattern pattern) {
+        if (pattern == null) {
+            return Collections.emptyList();
         }
-    };
+
+        List<ElmLowerCaseId> result = new LinkedList<ElmLowerCaseId>();
+
+        result.addAll(pattern.getLowerCaseIdList());
+
+        addDeclarationsToResult(
+                result,
+                pattern.getListPatternList(),
+                ElmTreeUtil::getDeclarationsFromParentPattern);
+
+        addDeclarationsToResult(
+                result,
+                pattern.getParenthesedPatternList(),
+                p -> getDeclarationsFromPattern(p.getPattern()));
+
+        addDeclarationsToResult(
+                result,
+                pattern.getRecordPatternList(),
+                ElmRecordPattern::getLowerCaseIdList);
+
+        addDeclarationsToResult(
+                result,
+                pattern.getTuplePatternList(),
+                ElmTreeUtil::getDeclarationsFromParentPattern);
+
+        addDeclarationsToResult(
+                result,
+                pattern.getUnionPatternList(),
+                ElmTreeUtil::getDeclarationsFromParentPattern);
+
+        return result;
+    }
 
     private static <T> void addDeclarationsToResult(List<ElmLowerCaseId> result, List<T> source, Function<T, List<ElmLowerCaseId>> f) {
-        List<ElmLowerCaseId> newElements =
-                ListUtils.flatten(
-                        ListUtils.map(source, f)
-                );
-        result.addAll(newElements);
+        source.stream()
+                .map(f)
+                .forEach(result::addAll);
     }
 
-    private static final Function<ElmParenthesedPattern, List<ElmLowerCaseId>> getDeclarationsFromParenthesedPattern =
-            new Function<ElmParenthesedPattern, List<ElmLowerCaseId>>() {
-                @Override
-                public List<ElmLowerCaseId> fun(ElmParenthesedPattern p) {
-                    return getDeclarationsFromPattern.fun(p.getPattern());
-                }
-            };
-
-    private static final Function<ElmRecordPattern, List<ElmLowerCaseId>> getDeclarationsFromRecord =
-            new Function<ElmRecordPattern, List<ElmLowerCaseId>>() {
-                @Override
-                public List<ElmLowerCaseId> fun(ElmRecordPattern recordPattern) {
-                    return recordPattern.getLowerCaseIdList();
-                }
-            };
-
-    private static <T extends ElmWithPatternList> Function<T, List<ElmLowerCaseId>> getDeclarationsFromParentPattern() {
-        return new Function<T, List<ElmLowerCaseId>>() {
-            @Override
-            public List<ElmLowerCaseId> fun(ElmWithPatternList parentPattern) {
-                List<ElmLowerCaseId> result = new LinkedList<ElmLowerCaseId>();
-                for (ElmPattern p : parentPattern.getPatternList()) {
-                    result.addAll(getDeclarationsFromPattern.fun(p));
-                }
-                return result;
-            }
-        };
+    private static <T extends ElmWithPatternList> List<ElmLowerCaseId> getDeclarationsFromParentPattern(ElmWithPatternList parentPattern) {
+        List<ElmLowerCaseId> result = new LinkedList<>();
+        parentPattern.getPatternList().stream()
+                .map(ElmTreeUtil::getDeclarationsFromPattern)
+                .forEach(result::addAll);
+        return result;
     }
-
-    private static final Function<ElmTuplePattern, List<ElmLowerCaseId>> getDeclarationsFromTuple =
-            getDeclarationsFromParentPattern();
-
-    private static final Function<ElmListPattern, List<ElmLowerCaseId>> getDeclarationsFromList =
-            getDeclarationsFromParentPattern();
-
-    private static final Function<ElmUnionPattern, List<ElmLowerCaseId>> getDeclarationsFromUnion =
-            getDeclarationsFromParentPattern();
-
-    private static final Function<ElmInnerValueDeclaration, List<ElmLowerCaseId>> getExposedDeclarationsFromInnerValue =
-            new Function<ElmInnerValueDeclaration, List<ElmLowerCaseId>>() {
-                @Override
-                public List<ElmLowerCaseId> fun(ElmInnerValueDeclaration innerValueDeclaration) {
-                    return getDeclarationsFromValue(innerValueDeclaration, true);
-                }
-            };
 }
