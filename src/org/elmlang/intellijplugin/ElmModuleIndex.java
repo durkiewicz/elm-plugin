8ccecd43dea39088011456e5ef551382437729b0
Kamil Durkiewicz <kamil.durkiewicz@avaus.fi>
2016-05-17 09:09:38 +0200

    Getting values and types from other files.

--- src/org/elmlang/intellijplugin/ElmModuleIndex.java
+++ src/org/elmlang/intellijplugin/ElmModuleIndex.java
@@ -15,6 +15,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 
 public class ElmModuleIndex extends ScalarIndexExtension<String> {
@@ -35,6 +36,10 @@ public class ElmModuleIndex extends ScalarIndexExtension<String> {
     @NotNull
     public static List<ElmFile> getFilesByModuleName(String moduleName, Project project) {
         return getFilesByModuleName(moduleName, project, GlobalSearchScope.projectScope(project));
+    }
+
+    public static Collection<String> getAllModuleNames(Project project) {
+        return FileBasedIndex.getInstance().getAllKeys(ELM_MODULE_INDEX, project);
     }
 
     @NotNull
--- src/org/elmlang/intellijplugin/features/completion/ElmCompletionProvider.java
+++ src/org/elmlang/intellijplugin/features/completion/ElmCompletionProvider.java
@@ -4,16 +4,21 @@ import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.util.Consumer;
 import com.intellij.util.ProcessingContext;
+import org.elmlang.intellijplugin.ElmModuleIndex;
 import org.elmlang.intellijplugin.psi.*;
 import org.elmlang.intellijplugin.psi.scope.ElmScope;
+import org.elmlang.intellijplugin.utils.TypeFilter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class ElmCompletionProvider extends CompletionProvider<CompletionParameters> {
@@ -55,12 +60,55 @@ class ElmCompletionProvider extends CompletionProvider<CompletionParameters> {
                     ElmScope.scopeFor((ElmLowerCaseId) parent),
                     resultSet
             );
-        } else if ((grandParent instanceof ElmMixedCasePath
-                || grandParent instanceof ElmUpperCasePath)
-                && parent.getStartOffsetInParent() == 0
-                ) {
-            addTypesCompletions((ElmFile) parent.getContainingFile(), resultSet);
+        } else if (grandParent instanceof ElmMixedCasePath
+                || grandParent instanceof ElmUpperCasePath) {
+            addTypeOrModuleCompletions(parent, resultSet);
         }
+    }
+
+    private static void addTypeOrModuleCompletions(PsiElement element, CompletionResultSet resultSet) {
+        ElmFile file = (ElmFile) element.getContainingFile();
+        if (element.getStartOffsetInParent() == 0) {
+            addTypesCompletions(file, resultSet);
+        } else if (Optional.ofNullable(element.getPrevSibling())
+                .flatMap(e -> Optional.ofNullable(e.getPrevSibling()))
+                .filter(e -> e instanceof ElmUpperCaseId)
+                .isPresent()) {
+            addModuleCompletions(file, element, resultSet);
+        }
+    }
+
+    private static void addModuleCompletions(ElmFile file, PsiElement element, CompletionResultSet resultSet) {
+        List<PsiElement> module = Arrays.stream(element.getParent().getChildren())
+                .filter(e -> e instanceof ElmUpperCaseId && e.getStartOffsetInParent() < element.getStartOffsetInParent())
+                .collect(Collectors.toList());
+        addModuleCompletions(file, module, resultSet);
+    }
+
+    private static void addModuleCompletions(ElmFile file, List<PsiElement> modulePrefix, CompletionResultSet resultSet) {
+        String moduleNamePrefix = modulePrefix.stream()
+                .map(PsiElement::getText)
+                .collect(Collectors.joining("."));
+        Project project = file.getProject();
+        ElmModuleIndex.getAllModuleNames(project).stream()
+                .filter(m -> m.startsWith(moduleNamePrefix))
+                .forEach(m -> addCompletionsForModule(m, moduleNamePrefix, project, resultSet));
+    }
+
+    private static void addCompletionsForModule(String moduleName, String modulePrefix, Project project, CompletionResultSet resultSet) {
+        if (moduleName.equals(modulePrefix)) {
+            ElmModuleIndex.getFilesByModuleName(moduleName, project).stream()
+                    .forEach(f -> addCompletionsForFile(f, resultSet));
+        }
+    }
+
+    private static void addCompletionsForFile(ElmFile file, CompletionResultSet resultSet) {
+        Stream.concat(
+                file.getExposedValues()
+                        .map(e -> (PsiElement)e),
+                file.getExposedTypes(TypeFilter.always(true))
+                        .map(e -> (PsiElement)e)
+        ).forEach(e -> addPsiElementToResult(e, resultSet));
     }
 
     private void addCompletionsInInvalidExpression(@NotNull PsiElement position, @NotNull CompletionResultSet resultSet) {
