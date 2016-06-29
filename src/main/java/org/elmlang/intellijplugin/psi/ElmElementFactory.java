8c80f43d024ef5f9a991b9935939d3712c3b465f
Kamil Durkiewicz <kamil.durkiewicz@avaus.fi>
2016-06-29 06:33:24 +0200

    Fixes for the rename refactoring to work.

--- src/main/java/org/elmlang/intellijplugin/psi/ElmElementFactory.java
+++ src/main/java/org/elmlang/intellijplugin/psi/ElmElementFactory.java
@@ -5,21 +5,6 @@ import com.intellij.psi.PsiFileFactory;
 import org.elmlang.intellijplugin.ElmFileType;
 
 public class ElmElementFactory {
-//    public static SimpleProperty createProperty(Project project, String name, String value) {
-//        final SimpleFile file = createFile(project, name + " = " + value);
-//        return (SimpleProperty) file.getFirstChild();
-//    }
-//
-//    public static SimpleProperty createProperty(Project project, String name) {
-//        final SimpleFile file = createFile(project, name);
-//        return (SimpleProperty) file.getFirstChild();
-//    }
-//
-//    public static PsiElement createCRLF(Project project) {
-//        final SimpleFile file = createFile(project, "\n");
-//        return file.getFirstChild();
-//    }
-
     public static ElmUpperCaseId createUpperCaseId(Project project, String text) {
         final ElmFile file = createFile(project, String.format("type Uniq%s=%s", text, text));
         return (ElmUpperCaseId) file.getFirstChild().getLastChild().getFirstChild();
@@ -27,7 +12,7 @@ public class ElmElementFactory {
 
     public static ElmLowerCaseId createLowerCaseId(Project project, String text) {
         final ElmFile file = createFile(project, String.format("%s=0", text));
-        return (ElmLowerCaseId) file.getFirstChild().getFirstChild();
+        return (ElmLowerCaseId) file.getFirstChild().getFirstChild().getFirstChild();
     }
 
     private static ElmFile createFile(Project project, String text) {
